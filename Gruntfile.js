05ec99a0c33cae9223bcceaae3e67f9bc1775862
MrPing <amsellem.jonathan@gmail.com>
2014-04-16 21:29:40 +0200

    grunt config copy images

--- Gruntfile.js
+++ Gruntfile.js
@@ -316,7 +316,7 @@ module.exports = function (grunt) {
                     src: [
                         '*.{ico,png,txt}',
                         '.htaccess',
-                        'images/{,*/}*.webp',
+                        'images/{,*/}*.*',
                         '{,*/}*.html',
                         'styles/fonts/{,*/}*.*'
                     ]
