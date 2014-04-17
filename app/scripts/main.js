370db656ad66f8cfc064c99d7bf4dbeb3edfef0c
MrPing <amsellem.jonathan@gmail.com>
2014-04-17 22:51:46 +0200

    control scroll

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,10 @@
 console.log('\'Allo \'Allo!');
+
+$('a[href^="#"]').click(function(){
+  var the_id = $(this).attr('href');
+
+  $('html, body').animate({
+    scrollTop:$(the_id).offset().top
+  }, 'slow');
+  return false;
+});
